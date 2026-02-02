7b80410de3f67fcdad5f896edfc554e08dc5b715
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2026-02-02 16:30:16 +0200

    implement SPA routing and add custom 404 redirect page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import DocsPage from '@/pages/DocsPage.vue'
 
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
